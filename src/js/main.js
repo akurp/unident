4c915bef22ccfb8925d11ff8a109bc0e3fa2577e
Alexey Kurpachyov <kurp96@ya.ru>
2020-01-07 14:14:10 +0300

    Сверстан блок с сотрудниками

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,27 @@ $(document).ready(function(){
     dots: true,
     arrows: false,
   });
+  $('.specs-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 991,
+      settings:
+      {
+        slidesToShow: 3
+      }},
+      {
+      breakpoint: 768,
+      settings:
+      {
+        slidesToShow: 2
+      }},
+      {
+      breakpoint: 567,
+      settings:
+      {
+        slidesToShow: 1
+      }
+    }]
+  });
 });
