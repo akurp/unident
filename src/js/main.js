9232e633fbce403105f65bab3c56f296130dd8bc
Alexey Kurpachyov <kurp96@ya.ru>
2020-01-09 16:51:58 +0300

    Сверстан блок с причиной

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,18 @@ $(document).ready(function(){
     dots: true,
     arrows: false,
   });
+  $('.reason-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.reason-arrow_prev'),
+    nextArrow: $('.reason-arrow_next')
+  });
   $('.specs-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
+    prevArrow: $('.specs-slide__arrow_prev'),
+    nextArrow: $('.specs-slide__arrow_next'),
     responsive: [{
       breakpoint: 991,
       settings:
