9621c77c87a4703879b558c1c77b120042677d3d
Alexey Kurpachyov <kurp96@ya.ru>
2020-01-15 01:16:30 +0300

    Макет сверстан с адаптивом.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  console.log($(window).width());
-  $('.slider').slick(
+    // sliders sets
+    $('.slider').slick(
         {slidesToShow: 1, slidesToScroll: 1, dots: true, arrows: false}
     );
     $('.reason-slider').slick(
@@ -17,7 +17,6 @@ $(document).ready(function () {
         prevArrow: $('.clients-arrow_prev'),
         dots: false,
         asNavFor: '.clients-slider-nav'
-
     });
     $('.clients-slider-nav').slick(
         {slidesToShow: 5, asNavFor: '.clients-slider', appendDots: $('.clients-slider-nav__item'), focusOnSelect: true}
@@ -50,6 +49,20 @@ $(document).ready(function () {
             }
         ]
     });
+    $('.reviews-slider').slick(
+        {slidesToShow: 1, slidesToScroll: 1, arrows: true, prevArrow: $('.reviews-arrow_prev'), nextArrow: $('.reviews-arrow_next')}
+    );
+
+    var e = $(".reviews")
+        .offset()
+        .top;
+    $(window).bind("scroll", function () {
+        $(this).scrollTop() > e && (
+            console.log("вот тут-то ты и попался, дружок-пирожок!"),
+            // $('#map1').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Acab7146d52ebf12a4a9dcbdd01bd93ec9b39bd82f9cd38b8b67eec4ac879fa74&amp;width=370&amp;height=255&amp;lang=ru_RU&amp;scroll=true"></script>'),
+            $(window).unbind("scroll")
+        )
+    })
 });
 // creating own audioplayer
 var myTrack = document.getElementById('myTrack');
@@ -60,85 +73,107 @@ var muteButton = document.getElementById('muteButton');
 var duration = document.getElementById('fullDuration');
 var currentTime = document.getElementById('currentTime');
 
-var width = document.getElementById('defaultBar').offsetWidth;
+var width = document
+    .getElementById('defaultBar')
+    .offsetWidth;
 
-var minutes = parseInt(myTrack.duration/60);
-var seconds = parseInt(myTrack.duration%60);
+var minutes = parseInt(myTrack.duration / 60);
+var seconds = parseInt(myTrack.duration % 60);
 
 var barSize = 520;
-
 
 var bar = document.getElementById('defaultBar');
 var progressBar = document.getElementById('progressBar');
 
 duration.innerHTML = minutes + ':' + seconds;
 
-playButton.addEventListener('click',playOrPause, false);
-muteButton.addEventListener('click',muteOrUnmute, false);
+playButton.addEventListener('click', playOrPause, false);
+muteButton.addEventListener('click', muteOrUnmute, false);
 
 function playOrPause() {
     if (!myTrack.paused && !myTrack.ended) {
-      myTrack.pause();
-      window.clearInterval(updateTime);
-
-        var elements = document.getElementsByClassName("fa fa-pause");
-        if (elements[0]) {
-          elements[0].className = "fa fa-play";
-          replaceClass();
-        }
-    } 
-    else {
+        myTrack.pause();
+        window.clearInterval(updateTime);
+        // var elements = document.getElementsByClassName("fa fa-pause"); if
+        // (elements[0]) {   elements[0].className = "fa fa-play";   replaceClass(); }
+        // document.getElementById('playIcon').classList.toggle("fa-pause");
+        document
+            .getElementById('playIcon')
+            .classList
+            .remove('fa-pause');
+        document
+            .getElementById('playIcon')
+            .classList
+            .add('fa-play');
+    } else {
         myTrack.play();
         updateTime = setInterval(update, 500);
-        var elements = document.getElementsByClassName("fa fa-play");
-        if (elements[0]) {
-          elements[0].className = "fa fa-pause";
-          replaceClass();
-        };
+        // var elements = document.getElementsByClassName("fa fa-play"); if
+        // (elements[0]) {   elements[0].className = "fa fa-pause";   replaceClass(); };
+        // document.getElementById('playIcon').classList.toggle("fa-play");
+        document
+            .getElementById('playIcon')
+            .classList
+            .remove('fa-play');
+        document
+            .getElementById('playIcon')
+            .classList
+            .add('fa-pause');
     }
 };
 
-function muteOrUnmute()
-{
-  if(myTrack.muted == true)
-  {
-    myTrack.muted = false;
-    var elements = document.getElementsByClassName("fa fa-volume-off");
-    if (elements[0]) {
-      elements[0].className = "fa fa-volume-up";
-      replaceClass();
+function muteOrUnmute() {
+    if (myTrack.muted == true) {
+        myTrack.muted = false;
+        // var elements = document.getElementsByClassName("fa fa-volume-off"); if
+        // (elements[0]) {   elements[0].className = "fa fa-volume-up"; replaceClass();
+        // }
+
+        document
+            .getElementById('volumeIcon')
+            .classList
+            .remove('fa-volume-off');
+        document
+            .getElementById('volumeIcon')
+            .classList
+            .add('fa-volume-up');
+    } else {
+        myTrack.muted = true;
+        // var elements = document.getElementsByClassName("fa fa-volume-up"); if
+        // (elements[0]) {   elements[0].className = "fa fa-volume-off"; replaceClass();
+        // }
+        document
+            .getElementById('volumeIcon')
+            .classList
+            .remove('fa-volume-up');
+        document
+            .getElementById('volumeIcon')
+            .classList
+            .add('fa-volume-off');
     }
-  }
-  else
-  {
-    myTrack.muted = true;
-    var elements = document.getElementsByClassName("fa fa-volume-up");
-    if (elements[0]) {
-      elements[0].className = "fa fa-volume-off";
-      replaceClass();
-    }
-  }
 }
 
-function update()
-{
-  if(!myTrack.ended)
-  {
-    var playedMinutes = parseInt(myTrack.currentTime/60);
-    var playedSeconds = parseInt(myTrack.currentTime%60);
-    currentTime.innerHTML = playedMinutes + ':' + playedSeconds;
-    var size = parseInt(myTrack.currentTime*barSize/myTrack.duration);
-    progressBar.style.width = size + "px";
-  }
-  else
-  {
-    currentTime.innerHTML = "0.00";
-    progressBar.style.width = "0px";
-    window.clearInterval(updateTime);
-    var elements = document.getElementsByClassName("fa fa-pause");
-    if (elements[0]) {
-      elements[0].className = "fa fa-play";
-      replaceClass();
+function update() {
+    if (!myTrack.ended) {
+        var playedMinutes = parseInt(myTrack.currentTime / 60);
+        var playedSeconds = parseInt(myTrack.currentTime % 60);
+        currentTime.innerHTML = playedMinutes + ':' + playedSeconds;
+        var size = parseInt(myTrack.currentTime * barSize / myTrack.duration);
+        progressBar.style.width = size + "px";
+    } else {
+        currentTime.innerHTML = "0.00";
+        progressBar.style.width = "0px";
+        window.clearInterval(updateTime);
+        // var elements = document.getElementsByClassName("fa fa-pause"); if
+        // (elements[0]) {   elements[0].className = "fa fa-play";   replaceClass(); }
+        document
+            .getElementById('playIcon')
+            .classList
+            .remove('fa-pause');
+        document
+            .getElementById('playIcon')
+            .classList
+            .add('fa-play');
+
     }
-  }
 }
